fa092c66227d734b8af3bb7c751f24e5e3e3055e
John Brunton <jbrunton@zipcar.com>
2017-05-06 15:16:29 +0100

    extract board store access to Store::Boards class

--- tasks/board.rb
+++ tasks/board.rb
@@ -4,34 +4,26 @@ require 'yaml/store'
 require './jira_api/client_builder'
 
 class Board < Thor
+  def initialize(*args)
+    super
+    @store = Store::Boards.instance
+  end
+
   desc "sync ID", "sync board"
   method_option :status, :aliases => "-s", :desc => "status"
   def sync(id)
-    store = board_store(id)
-
     status = options[:status]
     if status
-      store.transaction do
-        last_updated = store['last_updated'] || "Never"
+        last_updated = @store.board_last_updated(id) || "Never"
         puts "Last updated: #{last_updated}"
-      end
     else
       client = ClientBuilder.new.prompt.build
       rapid_view = client.RapidView.find(id)
       # rapid_views = client.RapidView.all.map do |rapid_view|
       #   [rapid_view.id, rapid_view.name]
       # end.to_h
-      store.transaction do
-        #boards_store['boards'] = rapid_views
-        store['last_updated'] = Time.now
-      end
+      @store.update_board(id)
       puts "Synced board"
     end
-  end
-
-private
-
-  def board_store(id)
-    YAML::Store.new("data/boards/#{id}.yml")
   end
 end
