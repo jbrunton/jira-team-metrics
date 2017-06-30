f5c75faa62fdd516e4fd4fd8cf84176d0740492b
John Brunton <jbrunton@zipcar.com>
2017-06-30 23:17:20 +0100

    add sync form and show progress bar

--- app/jobs/sync_domain_job.rb
+++ app/jobs/sync_domain_job.rb
@@ -3,12 +3,18 @@ class SyncDomainJob < ApplicationJob
 
   def perform(domain)
     puts "*** Syncing #{domain.name}"
+    ActionCable.server.broadcast(
+      "sync_status",
+      domain: domain.id,
+      status: 'syncing'
+    )
     sleep(5)
     puts "*** Done"
 
     ActionCable.server.broadcast(
       "sync_status",
-      domain: domain.id
+      domain: domain.id,
+      status: 'done'
     )
     # Do something later
   end
