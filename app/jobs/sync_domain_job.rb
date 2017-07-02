0c358bdf3363587a8090f1699f9d113200dc1150
John Brunton <jbrunton@zipcar.com>
2017-07-02 12:13:56 +0100

    refactor sync job

--- app/jobs/sync_domain_job.rb
+++ app/jobs/sync_domain_job.rb
@@ -3,41 +3,35 @@ class SyncDomainJob < ApplicationJob
 
   def perform(domain, username, password)
     #TODO: do this in a transaction
-    SyncDomainChannel.broadcast_to(
-      domain,
-      status: 'clearing cache',
-      in_progress: true
-    )
+    clear_cache(domain)
+    boards, statuses = fetch_data(domain, {username: username, password: password})
+    update_cache(domain, boards, statuses)
+    notify_complete(domain)
+  end
 
+private
+  def clear_cache(domain)
+    notify_status(domain, 'clearing cache')
     domain.boards.destroy_all
+  end
 
-    SyncDomainChannel.broadcast_to(
-      domain,
-      status: 'fetching from JIRA',
-      in_progress: true
-    )
-
-    client = JiraClient.new(domain.url, {username: username, password: password})
+  def fetch_data(domain, credentials)
+    notify_status(domain, 'fetching from JIRA')
+    client = JiraClient.new(domain.url, credentials)
     begin
       boards = client.get_rapid_boards
+      statuses = client.get_statuses
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
-      SyncDomainChannel.broadcast_to(
-        domain,
-        error: e.message,
-        errorCode: e.try(:response).try(:code),
-        in_progress: false
-      )
+      notify_error(domain, e.message, e.try(:response).try(:code))
       raise
     end
 
-    statuses = client.get_statuses
+    [boards, statuses]
+  end
 
-    SyncDomainChannel.broadcast_to(
-      domain,
-      status: 'updating cache',
-      in_progress: true
-    )
+  def update_cache(domain, boards, statuses)
+    notify_status(domain, 'updating cache')
 
     boards.each do |b|
       domain.boards.create(b)
@@ -46,11 +40,29 @@ class SyncDomainJob < ApplicationJob
     domain.last_synced = DateTime.now
     domain.statuses = statuses
     domain.save
+  end
 
+  def notify_status(domain, status)
+    SyncDomainChannel.broadcast_to(
+      domain,
+      status: status,
+      in_progress: true
+    )
+  end
+
+  def notify_complete(domain)
     SyncDomainChannel.broadcast_to(
       domain,
       in_progress: false
     )
-    # Do something later
+  end
+
+  def notify_error(domain, error, error_code)
+    SyncDomainChannel.broadcast_to(
+      domain,
+      error: error,
+      errorCode: error_code,
+      in_progress: false
+    )
   end
 end
