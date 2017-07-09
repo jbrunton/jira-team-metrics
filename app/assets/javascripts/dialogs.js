a602ede35245bfddbac16afc1a6695625a31a8a6
John Brunton <jbrunton@zipcar.com>
2017-07-09 10:40:16 +0100

    use fixed footer modal for domain config

--- app/assets/javascripts/dialogs.js
+++ app/assets/javascripts/dialogs.js
@@ -1,11 +1,52 @@
 function alertModal(opts) {
+  return modal({
+    title: opts.title,
+    body: '<p>' + opts.message + '</p>',
+    modalClass: 'modal',
+    template: 'alert'
+  });
+}
+
+function fixedFooterModal(opts) {
+  return modal(Object.assign({
+    modalClass: 'modal modal-fixed-footer',
+    template: 'modal'
+  }, opts));
+}
+
+function modal(opts) {
   var context = {
     title: opts.title,
-    message: opts.message
+    modalClass: opts.modalClass
   };
+  if (opts.bodyHtml) {
+    context.body = '<div id="modal-body"></div>';
+  } else {
+    context.body = opts.body;
+  }
 
-  var modal = $(render('dialogs/alert', context))
+  var modal = $(render('dialogs/' + opts.template, opts))
       .appendTo('body');
+
+  if (opts.render) {
+    opts.render(modal);
+  }
+
+  if (opts.bodyHtml) {
+    modal.find('#modal-body').html(opts.bodyHtml);
+  }
+
+  modal.find('.modal-ok').click(function() {
+    if (opts.confirm) {
+      opts.confirm();
+    }
+  });
+
+  modal.find('.modal-cancel').click(function() {
+    if (opts.cancel) {
+      opts.cancel();
+    }
+  });
 
   modal.modal({
     complete: function() {
