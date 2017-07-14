533dce36e470333966751c5141aa9b0bedd956da
John Brunton <jbrunton@zipcar.com>
2017-07-15 00:23:21 +0100

    add spinners

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,32 +1,48 @@
-function piechart(chartUrl, chartId) {
-  (function() {
-    function updateChart() {
-      $('#' + chartId).animate({ opacity: 0 })
-      var url = buildComponentUrl(chartUrl);
-      $.get(url, drawChart);
-    }
+function piechart(opts) {
+  var chartId = opts.id;
+  var chartUrl = opts.url;
+  var title = opts.title;
 
-    function drawChart(jsonData) {
-      var data = new google.visualization.DataTable(jsonData);
+  function chartDiv() {
+    return $('#' + chartId);
+  }
 
-      var options = {
-        legend: {
-          alignment: 'center'
-        }
-      };
-
-      var chartDiv = $('#' + chartId);
-      chartDiv.css('height', chartDiv.width() * 0.8);
-
-      var chart = new google.visualization.PieChart(document.getElementById(chartId));
-      chart.draw(data, options);
-      $('#' + chartId).animate({ opacity: 1 })
-    }
-
-    $(function () {
-      google.charts.setOnLoadCallback(updateChart);
-      $('#ct-states input').change(updateChart);
-      $('#ct-states textarea').change(updateChart);
+  function updateChart() {
+    chartDiv().animate({ opacity: 0 }, {
+      complete: function() {
+        $chartDiv = chartDiv();
+        $chartDiv.html(render('spinner'));
+        $chartDiv.animate({ opacity: 1 });
+      }
     });
-  })();
+    var url = buildComponentUrl(chartUrl);
+    $.get(url, drawChart);
+  }
+
+  function drawChart(jsonData) {
+    var data = new google.visualization.DataTable(jsonData);
+
+    var options = {
+      title: title,
+      chartArea: {
+        width: '70%'
+      },
+      legend: {
+        alignment: 'center'
+      }
+    };
+
+    $chartDiv = chartDiv();
+    $chartDiv.css('height', $chartDiv.width() * 0.7);
+
+    var chart = new google.visualization.PieChart(document.getElementById(chartId));
+    chart.draw(data, options);
+    $chartDiv.animate({ opacity: 1 })
+  }
+
+  $(function () {
+    google.charts.setOnLoadCallback(updateChart);
+    $('#ct-states input').change(updateChart);
+    $('#ct-states textarea').change(updateChart);
+  });
 }
