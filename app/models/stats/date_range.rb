c8e699230bf6e8ac5f29c74978b6b404b44b809c
John Brunton <jbrunton@zipcar.com>
2018-03-18 12:06:52 +0000

    add DateRange#overlap_with method and tests

--- app/models/stats/date_range.rb
+++ app/models/stats/date_range.rb
@@ -7,10 +7,17 @@ class DateRange
     @end_date = end_date
   end
 
+  def ==(other)
+    self.class == other.class &&
+      start_date == other.start_date &&
+      end_date == other.end_date
+  end
+  alias :eql? :==
+
   def to_a
     dates = []
-    next_date = dates.last || @start_date
-    while next_date < @end_date
+    next_date = dates.last || start_date
+    while next_date < end_date
       dates << next_date
       next_date = next_date + 1.day
     end
@@ -18,9 +25,21 @@ class DateRange
   end
 
   def overlaps?(other)
-    return false if @start_date.nil?
-    return false if @end_date.nil? || @end_date < other.start_date
-    return false if other.end_date < @start_date
-    true
+    return false if start_date.nil? || other.start_date.nil?
+    if end_date.nil?
+      return other.end_date.nil? || start_date < other.end_date
+    end
+    if other.end_date.nil?
+      return end_date.nil? || other.start_date < end_date
+    end
+    !(start_date > other.end_date || end_date < other.start_date)
+  end
+
+  def overlap_with(other)
+    return nil if start_date.nil? || other.start_date.nil?
+    return DateRange.new(
+      [start_date, other.start_date].max,
+      [end_date, other.end_date].compact.min
+    )
   end
 end
