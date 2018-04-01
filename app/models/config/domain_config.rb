f061f858f0e322949eb266a6bac0081d2493c1b3
John Brunton <jbrunton@zipcar.com>
2018-04-01 16:22:07 +0100

    add Rx schema validation for domain config

---
+++ app/models/config/domain_config.rb
@@ -0,0 +1,36 @@
+
+class DomainConfig
+  attr_reader :config_hash
+
+  def initialize(domain)
+    @config_hash = YAML.load(domain.config || '') || {}
+  end
+
+  def fields
+    config_hash['fields']
+  end
+
+  def validate
+    rx = Rx.new({ :load_core => true })
+    schema = rx.make_schema(YAML.load(SCHEMA))
+    schema.check!(config_hash)
+  end
+
+  SCHEMA = <<~END
+    type: "//rec"
+    optional:
+      fields:
+        type: "//arr"
+        contents: "//str"
+      link_types:
+        type: "//arr"
+        contents: "//str"
+      increments:
+        type: "//arr"
+        contents:
+          type: "//rec"
+          required:
+            issue_type: "//str"
+            inward_link_type: "//str"
+  END
+end
