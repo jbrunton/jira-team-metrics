c312064ea7fc57a7aedd3c6a984761d95638daf9
John Brunton <jbrunton@zipcar.com>
2018-04-04 00:13:34 +0100

    messing around with CFDs

--- app/models/stats/increment_report.rb
+++ app/models/stats/increment_report.rb
@@ -41,4 +41,39 @@ class IncrementReport
       Time.now + (remaining_issues.count / completion_rate).days
     end
   end
+
+  def cfd_data(from_date)
+    data = [['Day', 'To Do', 'In Progress', 'Done']]
+    dates = DateRange.new(from_date, Time.now).to_a
+    dates.each_with_index do |date, index|
+      row = [index]
+
+      states = cfd_states_on(date)
+
+      row << states['To Do']
+      row << states['In Progress']
+      row << states['Done']
+
+      data << row
+    end
+    data
+  end
+
+  def cfd_states_on(date)
+    states = {
+      'To Do' => 0,
+      'In Progress' => 0,
+      'Done' => 0
+    }
+    @issues.each do |issue|
+      if issue.issue_created < date && (issue.started_time.nil? || date < issue.started_time)
+        states['To Do'] += 1
+      elsif issue.started_time && issue.started_time < date && (issue.completed_time.nil? || date < issue.completed_time)
+        states['In Progress'] += 1
+      else
+        states['Done'] += 1
+      end
+    end
+    states
+  end
 end
