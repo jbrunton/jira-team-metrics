9d29431198fc3bf80437ab4f85f2a2694fbefaa2
John Brunton <jbrunton@zipcar.com>
2018-04-15 21:10:14 +0100

    add CONFIG_URL env handler

--- lib/jira_team_metrics/engine.rb
+++ lib/jira_team_metrics/engine.rb
@@ -15,5 +15,19 @@ module JiraTeamMetrics
     config.autoload_paths << File.expand_path("../../../app/models/jira_team_metrics/stats", __FILE__)
     config.autoload_paths << File.expand_path("../../../app/models/jira_team_metrics/stats/scope", __FILE__)
     config.autoload_paths << File.expand_path("../../../app/models/jira_team_metrics/reports", __FILE__)
+
+    config.after_initialize do
+      unless ENV['CONFIG_URL'].nil?
+        log_message = "CONFIG_URL defined. Setting config from #{ENV['CONFIG_URL']}"
+        Rails.logger.info log_message
+        puts log_message # Rails doesn't print to stdout during boot
+
+        domain = JiraTeamMetrics::Domain.get_instance
+        domain.config_string = open(ENV['CONFIG_URL']).read
+        unless domain.save
+          raise 'Invalid config: ' + domain.errors.full_messages.join(',')
+        end
+      end
+    end
   end
 end
