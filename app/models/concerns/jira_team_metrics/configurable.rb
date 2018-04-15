ea988c11e2c559b258c4e1b87804b0ed596afa4f
John Brunton <jbrunton@zipcar.com>
2018-04-15 21:04:29 +0100

    syntax tweaks

--- app/models/concerns/jira_team_metrics/configurable.rb
+++ app/models/concerns/jira_team_metrics/configurable.rb
@@ -1,27 +1,25 @@
-module JiraTeamMetrics
-  module Configurable
-    extend ActiveSupport::Concern
+module JiraTeamMetrics::Configurable
+  extend ActiveSupport::Concern
 
-    included do
-      validate :validate_config
-    end
+  included do
+    validate :validate_config
+  end
 
-    def config
-      config_class = "#{self.class.name}Config".constantize
-      config_class.new(config_hash)
-    end
+  def config
+    config_class = "#{self.class.name}Config".constantize
+    config_class.new(config_hash)
+  end
 
-  private
-    def config_hash
-      YAML.load(config_string || '') || {}
-    end
+private
+  def config_hash
+    YAML.load(config_string || '') || {}
+  end
 
-    def validate_config
-      begin
-        config.validate
-      rescue Rx::ValidationError => e
-        errors.add(:config, e.message)
-      end
+  def validate_config
+    begin
+      config.validate
+    rescue Rx::ValidationError => e
+      errors.add(:config, e.message)
     end
   end
 end
