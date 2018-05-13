4769fda4c6607523cc503666a99794db0bb2a326
John Brunton <jbrunton@zipcar.com>
2018-05-13 19:56:57 +0100

    add TeamAdjustment struct

---
+++ app/scope/jira_team_metrics/team_adjustment.rb
@@ -0,0 +1,34 @@
+JiraTeamMetrics::TeamAdjustment = Struct.new(:adjustments) do
+  def adjusted_epic_scope(trained_epic_scope)
+    if epic_scope.nil?
+      trained_epic_scope * epic_scope_factor
+    else
+      epic_scope
+    end
+  end
+
+  def adjusted_throughput(trained_throughput)
+    if throughput.nil?
+      trained_throughput * throughput_factor
+    else
+      throughput
+    end
+  end
+
+private
+  def epic_scope
+    adjustments[:epic_scope]
+  end
+
+  def epic_scope_factor
+    adjustments[:epic_scope_factor] || 1.0
+  end
+
+  def throughput
+    adjustments[:throughput]
+  end
+
+  def throughput_factor
+    adjustments[:throughput_factor] || 1.0
+  end
+end
---
+++ spec/scope/jira_team_metrics/team_adjustment_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe JiraTeamMetrics::TeamAdjustment do
+  let(:adjustments) { {} }
+
+  describe "#adjusted_epic_scope" do
+    context "when no adjustments are given" do
+      it "returns the trained epic scope" do
+        team_adjustment = JiraTeamMetrics::TeamAdjustment.new(adjustments)
+        expect(team_adjustment.adjusted_epic_scope(2.0)).to eq(2.0)
+      end
+    end
+
+    context "when an epic scope value is given" do
+      before(:each) { adjustments[:epic_scope] = 3.0 }
+
+      it "returns the given epic scope" do
+        team_adjustment = JiraTeamMetrics::TeamAdjustment.new(adjustments)
+        expect(team_adjustment.adjusted_epic_scope(2.0)).to eq(3.0)
+      end
+    end
+
+    context "when an epic scope factor is given" do
+      before(:each) { adjustments[:epic_scope_factor] = 2.0 }
+
+      it "returns the adjusted epic scope" do
+        team_adjustment = JiraTeamMetrics::TeamAdjustment.new(adjustments)
+        expect(team_adjustment.adjusted_epic_scope(2.0)).to eq(4.0)
+      end
+    end
+  end
+
+  describe "#adjusted_throughput" do
+    context "when no adjustments are given" do
+      it "returns the trained throughput" do
+        team_adjustment = JiraTeamMetrics::TeamAdjustment.new(adjustments)
+        expect(team_adjustment.adjusted_throughput(2.0)).to eq(2.0)
+      end
+    end
+
+    context "when an epic scope value is given" do
+      before(:each) { adjustments[:throughput] = 3.0 }
+
+      it "returns the given epic scope" do
+        team_adjustment = JiraTeamMetrics::TeamAdjustment.new(adjustments)
+        expect(team_adjustment.adjusted_throughput(2.0)).to eq(3.0)
+      end
+    end
+
+    context "when an epic scope factor is given" do
+      before(:each) { adjustments[:throughput_factor] = 2.0 }
+
+      it "returns the adjusted epic scope" do
+        team_adjustment = JiraTeamMetrics::TeamAdjustment.new(adjustments)
+        expect(team_adjustment.adjusted_throughput(2.0)).to eq(4.0)
+      end
+    end
+  end
+end
