77e1a93284cd5fa00a04b003ea39f2b5d555b28d
John Brunton <jbrunton@zipcar.co.uk>
2018-05-26 16:19:31 -0400

    show moving average

--- app/charts/jira_team_metrics/throughput_chart.rb
+++ app/charts/jira_team_metrics/throughput_chart.rb
@@ -9,18 +9,28 @@ class JiraTeamMetrics::ThroughputChart
   def data_table
     completed_issues = @board.completed_issues(@params.date_range)
     filtered_issues = JiraTeamMetrics::MqlInterpreter.new(@board, completed_issues).eval(@params.query)
+    dates = @params.date_range.to_a
+
     data_table = JiraTeamMetrics::DataTableBuilder.new
         .data(filtered_issues)
         .pick(:completed_time, :key)
         .build
-
-    dates = @params.date_range.to_a
-
-    data_table
         .select('completed_time').count('key', as: 'Count')
         .group(if_nil: 0) { |completed_time| completed_time.to_date }
         .sort_by('completed_time')
         .insert_if_missing(dates, [0])
+
+    th_counts = data_table.column_values('Count')
+    th_averages = th_counts.count.times.map do |index|
+      start = index - 13
+      length = 14
+      while start < 0
+        start += 1
+        length -= 1
+      end
+      th_counts.slice(start, length).mean
+    end
+    data_table.add_column('Average', th_averages)
   end
 
   def json_data
