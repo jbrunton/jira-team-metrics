cfed9b5d762fa983ad8220b80089b98551944275
John Brunton <jbrunton@zipcar.com>
2018-06-02 21:05:32 +0100

    add specs for FormattingHelper

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :abort_if_syncing
+
+private
+  def abort_if_syncing
+    byebug
+  end
 end
