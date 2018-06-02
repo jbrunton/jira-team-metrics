ac8bee3c8445fd747a18154b86583c3b1c5e1cc7
John Brunton <jbrunton@zipcar.com>
2018-06-02 21:16:05 +0100

    remove filter

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_filter :abort_if_syncing
-
-private
-  def abort_if_syncing
-    byebug
-  end
 end
