28ad8c4826e6e22928bf865d5054d5b82c85cbcb
John Brunton <jbrunton@zipcar.com>
2018-06-03 10:45:44 +0100

    fix tests

--- spec/helpers/jira_team_metrics/application_helper_spec.rb
+++ spec/helpers/jira_team_metrics/application_helper_spec.rb
@@ -11,22 +11,4 @@ RSpec.describe JiraTeamMetrics::ApplicationHelper do
       expect(helper.readonly?).to eq(false)
     end
   end
-
-  describe "#syncing?" do
-    let(:board) { create(:board) }
-
-    it "returns true if object.syncing is true" do
-      board.syncing = true
-      expect(helper.syncing?(board)).to eq(true)
-    end
-
-    it "returns false if object.syncing is false" do
-      board.syncing = false
-      expect(helper.syncing?(board)).to eq(false)
-    end
-
-    it "returns true if passed nil" do
-      expect(helper.syncing?(nil)).to eq(false)
-    end
-  end
 end
