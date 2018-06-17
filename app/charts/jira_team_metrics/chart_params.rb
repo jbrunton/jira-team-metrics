e394c74832c6c4cf6967bc0e02102af28867370a
John Brunton <jbrunton@zipcar.com>
2018-06-17 20:49:47 +0100

    set default from_date to -30 days for charts

--- app/charts/jira_team_metrics/chart_params.rb
+++ app/charts/jira_team_metrics/chart_params.rb
@@ -11,7 +11,7 @@ class JiraTeamMetrics::ChartParams
 
   def self.from_params(board, params)
     if params[:from_date].blank?
-      from_date = (board.synced_from || DateTime.now - 90)
+      from_date = DateTime.now - 30
     else
       from_date = DateTime.parse(params[:from_date])
     end
