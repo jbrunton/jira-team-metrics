dd0f4604dae35ace1b714382953950d525f36a67
John Brunton <jbrunton@zipcar.com>
2018-07-05 21:03:52 +0100

    extract ConfigFileService and rename CONFIG_URL -> CONFIG_FILE

---
+++ spec/services/jira_team_metrics/config_file_service.rb
@@ -0,0 +1,22 @@
+class JiraTeamMetrics::ConfigFileService
+  attr_reader :domain
+  attr_reader :config_file
+
+  def initialize(config_file)
+    @config_file = config_file
+    @domain = JiraTeamMetrics::Domain.get_instance
+  end
+
+  def load_config
+    unless @config_file.blank?
+      log_message = "CONFIG_FILE defined. Setting config from #{@config_file}"
+      Rails.logger.info log_message
+      puts log_message # Rails doesn't print to stdout during boot
+
+      @domain.config_string = open(@config_file).read
+      unless @domain.save
+        raise 'Invalid config: ' + @domain.errors.full_messages.join(',')
+      end
+    end
+  end
+end
