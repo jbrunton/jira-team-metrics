846379002e08c5be2381c1abb750de011e6f00e4
John Brunton <jbrunton@zipcar.com>
2018-07-15 16:38:21 +0100

    add tests for late case and trained forecasts case

--- spec/scope/jira_team_metrics/status_analyzer_spec.rb
+++ spec/scope/jira_team_metrics/status_analyzer_spec.rb
@@ -16,13 +16,26 @@ RSpec.describe JiraTeamMetrics::StatusAnalyzer do
   describe "#analyzer" do
     context "when done" do
       before(:each) do
-        expect(status_analyzer).to receive(:remaining_scope).and_return([])
+        allow(status_analyzer).to receive(:remaining_scope).and_return([])
       end
 
       it "returns a green status" do
         status_analyzer.analyze
         expect(status_analyzer.status_color).to eq('blue')
         expect(status_analyzer.status_reason).to eq('Done.')
+      end
+    end
+
+    context "when target date is in the past" do
+      before(:each) do
+        project.fields['Target Date'] = '2018-05-01'
+        allow(status_analyzer).to receive(:remaining_scope).and_return([create(:issue)])
+      end
+
+      it "returns a red status" do
+        status_analyzer.analyze
+        expect(status_analyzer.status_color).to eq('red')
+        expect(status_analyzer.status_reason).to eq('Target date is in the past.')
       end
     end
 
@@ -69,6 +82,20 @@ RSpec.describe JiraTeamMetrics::StatusAnalyzer do
           expect(status_analyzer.status_color).to eq('red')
           expect(status_analyzer.status_reason).to eq('Using rolling forecast. Forecast is at risk, over target by 20% of time remaining.')
         end
+      end
+    end
+
+    context "when using trained forecasts" do
+      before(:each) do
+        expect(team_report).to receive(:completed_scope).and_return([])
+        allow(team_report).to receive(:remaining_scope).and_return([create(:issue)])
+        allow(team_report).to receive(:forecast_completion_date).and_return(today + 15)
+      end
+
+      it "changes the status reason accordingly" do
+        status_analyzer.analyze
+        expect(status_analyzer.status_color).to eq('green')
+        expect(status_analyzer.status_reason).to eq('< 5 issues completed, using predicted forecast. Forecast is on target.')
       end
     end
   end
