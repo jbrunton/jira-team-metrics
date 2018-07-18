8144d504243552734c8d4aacbf93ab672fcac0bb
John Brunton <jbrunton@zipcar.com>
2018-07-18 22:24:17 +0100

    add tests for throughput and forecast methods

--- spec/models/jira_team_metrics/epic_spec.rb
+++ spec/models/jira_team_metrics/epic_spec.rb
@@ -10,6 +10,8 @@ describe JiraTeamMetrics::Epic do
   let!(:issue3) { create(:issue, epic: epic, started_time: today - 5) }
   let!(:issue4) { create(:issue, epic: epic) }
 
+  before(:each) { travel_to today }
+
   describe "#percent_done" do
     it "returns the percentage of completed issues" do
       expect(epic.percent_done).to eq(50.0)
@@ -37,6 +39,18 @@ describe JiraTeamMetrics::Epic do
   describe "#remaining_scope" do
     it "returns the remaining issues for the epic" do
       expect(epic.remaining_scope).to eq([issue3, issue4])
+    end
+  end
+
+  describe "#throughput" do
+    it "returns the average throughput for the epic so far" do
+      expect(epic.throughput).to eq(0.2) # 2 issues in 10 days 0.2 / day
+    end
+  end
+
+  describe "#forecast" do
+    it "forecasts the completion date" do
+      expect(epic.forecast).to eq(today + 10) # 2 remaining issues at 0.2 / day
     end
   end
 end
