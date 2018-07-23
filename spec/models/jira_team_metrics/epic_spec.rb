d7249a5b31ca051c76877f5627483a9e819cdddb
John Brunton <jbrunton@zipcar.com>
2018-07-23 21:37:29 +0100

    fix specs for Epic

--- spec/models/jira_team_metrics/epic_spec.rb
+++ spec/models/jira_team_metrics/epic_spec.rb
@@ -12,61 +12,16 @@ describe JiraTeamMetrics::Epic do
 
   before(:each) { travel_to today }
 
-  describe "#percent_done" do
-    it "returns the percentage of completed issues" do
-      expect(epic.percent_done).to eq(50.0)
-    end
-  end
-
   describe "#scope" do
     it "returns the scope for the epic" do
       expect(epic.scope).to eq([issue1, issue2, issue3, issue4])
     end
   end
 
-  describe "#completed_scope" do
-    it "returns the completed issues for the epic" do
-      expect(epic.completed_scope).to eq([issue1, issue2])
-    end
-  end
-
-  describe "#in_progress_scope" do
-    it "returns the in progress issues for the epic" do
-      expect(epic.in_progress_scope).to eq([issue3])
-    end
-  end
-
-  describe "#remaining_scope" do
-    it "returns the remaining issues for the epic" do
-      expect(epic.remaining_scope).to eq([issue3, issue4])
-    end
-  end
-
-  describe "#throughput" do
-    context "when passed nil" do
-      it "returns the average throughput for the epic so far" do
-        expect(epic.throughput(nil)).to eq(0.2) # 2 issues in 10 days = 0.2 / day
-      end
-    end
-
-    context "when passed an integer" do
-      it "returns the throughput for the given rolling window" do
-        expect(epic.throughput(5)).to eq(0.4) # 2 issues in 5 days = 0.4 / day
-      end
-    end
-  end
-
-  describe "#forecast" do
-    context "when passed nil" do
-      it "forecasts the completion date based on the throughput so far" do
-        expect(epic.forecast(nil)).to eq(today + 10) # 2 remaining issues at 0.2 / day
-      end
-    end
-
-    context "when passed an integer" do
-      it "forecasts the completion date based on the throughput for the given window" do
-        expect(epic.forecast(5)).to eq(today + 5) # 2 remaining issues at 0.4 / day
-      end
+  describe "#forecaster" do
+    it "returns a forecaster for the epic" do
+      expect(epic.forecaster.class).to eq(JiraTeamMetrics::Forecaster)
+      expect(epic.forecaster.scope).to eq(epic.scope)
     end
   end
 end
