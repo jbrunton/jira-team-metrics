ab8a94ed9c59ae9176ad735ea204caf0935fe884
John Brunton <jbrunton@zipcar.com>
2018-07-28 12:48:20 +0100

    link parent issues

--- app/services/jira_team_metrics/issue_linker_service.rb
+++ app/services/jira_team_metrics/issue_linker_service.rb
@@ -4,6 +4,12 @@ class JiraTeamMetrics::IssueLinkerService
   end
 
   def build_graph
+    link_epics
+    link_parents
+  end
+
+private
+  def link_epics
     @board.issues.each do |issue|
       epic_key = issue.fields['Epic Link']
       unless epic_key.blank?
@@ -12,5 +18,26 @@ class JiraTeamMetrics::IssueLinkerService
         issue.save
       end
     end
+  end
+
+  def link_parents
+    project_type = @board.domain.config.project_type
+    return if project_type.nil?
+
+    @board.issues.each do |issue|
+      parent_key = find_parent_key(issue, project_type)
+      unless parent_key.blank?
+        issue.parent_key = parent_key
+        issue.parent = @board.issues.find_by(key: parent_key)
+        issue.save
+      end
+    end
+  end
+
+  def find_parent_key(issue, project_type)
+    parent_link = issue.links
+      .find{ |link| link['inward_link_type'] == project_type.inward_link_type }
+
+    parent_link['issue']['key'] unless parent_link.nil?
   end
 end
