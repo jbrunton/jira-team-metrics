9970813d7c68665ec5e0fe7d075779c38b126554
John Brunton <jbrunton@zipcar.co.uk>
2018-09-29 09:55:19 -0400

    extract reports_config Rx type

--- app/config/jira_team_metrics/base_config.rb
+++ app/config/jira_team_metrics/base_config.rb
@@ -8,6 +8,9 @@ class JiraTeamMetrics::BaseConfig
 
   def validate
     rx = Rx.new({ :load_core => true })
+    rx.add_prefix('metrics', 'jira-team-metrics/')
+    reports_schema_path = File.join(__dir__, 'schemas/types', 'reports_config.yml')
+    rx.learn_type('jira-team-metrics/reports-config', YAML.load_file(reports_schema_path))
     schema_path = File.join(__dir__, 'schemas', "#{@schema_name}.yml")
     schema = rx.make_schema(YAML.load_file(schema_path))
     schema.check!(config_hash)
