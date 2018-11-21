5fe88a921f83b13095bd31923f04646687f840c9
John Brunton <jbrunton@zipcar.co.uk>
2018-11-21 20:32:21 +0000

    move MqlLexer into parsers directory

--- lib/jira_team_metrics/engine.rb
+++ lib/jira_team_metrics/engine.rb
@@ -17,6 +17,8 @@ module JiraTeamMetrics
     require 'pickadate-rails'
     require 'gretel'
 
+    config.autoload_paths << "#{config.root}/app/mql/parsers"
+
     config.after_initialize do
       unless ActiveRecord::Base.connection.migration_context.needs_migration?
         JiraTeamMetrics::DatabaseService.new.prepare_database
