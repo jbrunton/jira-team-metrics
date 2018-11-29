36989e7f1c7ed556b4acf344762b93739047e487
John Brunton <jbrunton@zipcar.com>
2018-11-29 21:39:30 +0000

    fix some failing tests

--- spec/factories/domain_factory.rb
+++ spec/factories/domain_factory.rb
@@ -7,13 +7,15 @@ FactoryBot.define do
     end
 
     before(:create) do |domain, evaluator|
-      domain.config_string = <<~CONFIG
-        url: https://jira.example.com
-        projects:
-          issue_type: #{evaluator.project_issue_type}
-          inward_link_type: is included in
-          outward_link_type: includes
-      CONFIG
+      if domain.config_string.nil?
+        domain.config_string = <<~CONFIG
+          url: https://jira.example.com
+          projects:
+            issue_type: #{evaluator.project_issue_type}
+            inward_link_type: is included in
+            outward_link_type: includes
+        CONFIG
+      end
     end
   end
 end
