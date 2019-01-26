b000a3bde0e740391ac9ca4bc5eb700aa017fb75
John Brunton <jbrunton@zipcar.com>
2019-01-26 22:27:21 +0000

    simplify test cases

--- app/config/jira_team_metrics/config.rb
+++ app/config/jira_team_metrics/config.rb
@@ -1,9 +1,9 @@
 class JiraTeamMetrics::Config
   attr_reader :config_hash
 
-  def initialize(config_hash, schema_name)
+  def initialize(config_hash, schema)
     @config_hash = config_hash
-    @schema_name = schema_name
+    @schema = schema
   end
 
   def validate
@@ -11,8 +11,24 @@ class JiraTeamMetrics::Config
     rx.add_prefix('metrics', 'jira-team-metrics/')
     reports_schema_path = File.join(__dir__, 'schemas/types', 'reports_config.yml')
     rx.learn_type('jira-team-metrics/reports-config', YAML.load_file(reports_schema_path))
-    schema_path = File.join(__dir__, 'schemas', "#{@schema_name}.yml")
-    schema = rx.make_schema(YAML.load_file(schema_path))
+    schema = rx.make_schema(@schema)
     schema.check!(config_hash)
+  end
+
+  def self.config_for(object)
+    case object.class
+      when JiraTeamMetrics::Domain
+        schema_path = File.join(__dir__, 'schemas', 'domain_config.yml')
+      when JiraTeamMetrics::Board
+        schema_path = File.join(__dir__, 'schemas', 'board_config.yml')
+      else
+        raise "Unexpected class: #{object.class}"
+    end
+    schema = YAML.load_file(schema_path)
+    JiraTeamMetrics::Config.new(object.config_hash, schema)
+  end
+
+  def self.domain_config(config_hash)
+    JiraTeamMetrics::Config.new(config_hash, 'board_config')
   end
 end
--- spec/config/jira_team_metrics/config_spec.rb
+++ spec/config/jira_team_metrics/config_spec.rb
@@ -1,58 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe JiraTeamMetrics::Config do
-  let(:default_query) { "not (filter = 'Outliers')" }
-  let(:board_reports) do
+  let(:config_hash) do
     {
-      'epics' => {
-        'sections' => [
-          {
-            'title' => 'Board In Progress',
-            'mql' => "status = 'In Progress'"
-          }
-        ]
-      },
-      'projects' => {
-        'sections' => [
-          {
-            'title' => 'Board In Progress',
-            'mql' => "status = 'In Progress'"
-          }
-        ]
-      },
-      'scatterplot' => {
-        'default_query' => 'board scatterplot default query'
-      },
-      'throughput' => {
-        'default_query' => 'board throughput default query'
-      },
-      'aging_wip' => {
-        'completed_query' => 'board aging_wip completed query'
-      }
+      'foo' => 'bar'
     }
   end
 
-  let(:config_hash) do
-    {
-      'default_query' => default_query,
-      'reports' => board_reports
-    }
+  let(:schema) do
+    <<~SCHEMA
+    type: "//rec"
+    optional:
+      foo:
+        type: "//str"
+    SCHEMA
   end
 
   it "initializes #config_hash" do
-    config = JiraTeamMetrics::Config.new(config_hash, 'board_config')
+    config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
     expect(config.config_hash).to eq(config_hash)
   end
 
   context "#validate" do
     it "validates a well formed config" do
-      config = JiraTeamMetrics::Config.new(config_hash, 'board_config')
+      config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
       expect { config.validate }.not_to raise_error
     end
 
     it "validates the top level fields" do
       config_hash['unexpected_field'] = 'foo'
-      config = JiraTeamMetrics::Config.new(config_hash, 'board_config')
+      config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
       expect { config.validate }.to raise_error(Rx::ValidationError, /Hash had extra keys/)
     end
   end
