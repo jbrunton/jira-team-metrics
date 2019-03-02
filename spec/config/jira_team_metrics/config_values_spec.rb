12c903ad098ab2cdc8e38d4f62db82d36772ac9d
John Brunton <jbrunton@zipcar.com>
2019-03-02 18:36:29 +0000

    test ConfigValues directly

--- spec/config/jira_team_metrics/config_values_spec.rb
+++ spec/config/jira_team_metrics/config_values_spec.rb
@@ -34,45 +34,45 @@ RSpec.describe JiraTeamMetrics::ConfigValues do
 
   context "#method_missing" do
     it "raises an error if the field isn't defined" do
-      config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+      config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
       expect { config.baz }.to raise_error(ArgumentError, 'Unknown config key: baz')
     end
 
     context "scalar values" do
       it "returns scalar values" do
-        config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+        config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
         expect(config.foo).to eq(123)
       end
 
       it "returns null for empty values" do
         config_hash.delete('foo')
-        config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+        config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
         expect(config.foo).to eq(nil)
       end
     end
 
     context "nested values" do
       it "returns nested values" do
-        config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+        config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
         expect(config.bar.baz).to eq('qux')
       end
 
       it "returns null for empty nested values" do
         config_hash['bar'].delete('baz')
-        config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+        config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
         expect(config.bar.baz).to eq(nil)
       end
     end
 
     context "array values" do
       it "returns nested values" do
-        config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+        config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
         expect(config.foos.to_a).to eq([123])
       end
 
       it "returns an empty array for empty values" do
         config_hash.delete('foos')
-        config = JiraTeamMetrics::Config.new(config_hash, YAML.load(schema))
+        config = JiraTeamMetrics::ConfigValues.new(config_hash, YAML.load(schema), nil)
         expect(config.foos.to_a).to eq([])
       end
     end
