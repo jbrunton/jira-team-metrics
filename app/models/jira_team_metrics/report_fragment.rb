6faf58186cab7ad2eccc8c6a95ea395222aab59c
John Brunton <jbrunton@zipcar.com>
2019-05-06 22:41:14 +0100

    limit fragment lookup to given jira_id

--- app/models/jira_team_metrics/report_fragment.rb
+++ app/models/jira_team_metrics/report_fragment.rb
@@ -2,10 +2,13 @@ class JiraTeamMetrics::ReportFragment < ApplicationRecord
   serialize :contents
   belongs_to :sync_history
 
-  def self.fetch(report_key, fragment_key, sync_history_id = nil)
+  # TODO: join with board_id
+  def self.fetch(jira_id, report_key, fragment_key, sync_history_id = nil)
     if sync_history_id.nil?
       JiraTeamMetrics::ReportFragment
+        .joins(:sync_history)
         .where(report_key: report_key, fragment_key: fragment_key)
+        .where(jira_team_metrics_sync_histories: { jira_board_id: jira_id })
         .order(created_at: :desc)
         .first
     else
@@ -14,8 +17,8 @@ class JiraTeamMetrics::ReportFragment < ApplicationRecord
     end
   end
 
-  def self.fetch_contents(report_key, fragment_key, sync_history_id = nil)
-    fragment = fetch(report_key, fragment_key, sync_history_id)
+  def self.fetch_contents(jira_id, report_key, fragment_key, sync_history_id = nil)
+    fragment = fetch(jira_id, report_key, fragment_key, sync_history_id)
     fragment.contents unless fragment.nil?
   end
 end
--- spec/models/jira_team_metrics/report_fragment_spec.rb
+++ spec/models/jira_team_metrics/report_fragment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe JiraTeamMetrics::ReportFragment, type: :model do
   let(:created_time) { DateTime.now }
 
   let(:my_fragment_contents) { 'Fragment Contents' }
-  let(:sync_history) { JiraTeamMetrics::SyncHistory.create }
+  let(:sync_history) { JiraTeamMetrics::SyncHistory.create(jira_board_id: jira_id) }
   let!(:my_fragment) {
     travel_to created_time do
       JiraTeamMetrics::ReportFragment.create(
@@ -17,7 +17,7 @@ RSpec.describe JiraTeamMetrics::ReportFragment, type: :model do
   }
 
   let(:my_old_fragment_contents) { 'Old Fragment Contents' }
-  let(:old_sync_history) { JiraTeamMetrics::SyncHistory.create }
+  let(:old_sync_history) { JiraTeamMetrics::SyncHistory.create(jira_board_id: jira_id) }
   let!(:my_old_fragment) do
     travel_to created_time - 1 do
       JiraTeamMetrics::ReportFragment.create(
@@ -29,30 +29,52 @@ RSpec.describe JiraTeamMetrics::ReportFragment, type: :model do
   end
 
   describe ".fetch" do
-    it "returns the latest fragment for the given report_key and fragment_key" do
-      fragment = JiraTeamMetrics::ReportFragment.fetch('my_report', 'my_fragment')
+    it "returns the latest fragment for the given keys" do
+      fragment = JiraTeamMetrics::ReportFragment.fetch(jira_id, 'my_report', 'my_fragment')
       expect(fragment).to eq(my_fragment)
     end
 
     it "returns the latest fragment for the matching sync_history_id when given" do
-      fragment = JiraTeamMetrics::ReportFragment.fetch('my_report', 'my_fragment', old_sync_history.id)
+      fragment = JiraTeamMetrics::ReportFragment.fetch(jira_id, 'my_report', 'my_fragment', old_sync_history.id)
       expect(fragment).to eq(my_old_fragment)
+    end
+
+    context "if there are matching reports for another board" do
+      let(:other_jira_id) { jira_id + 1 }
+      let(:other_sync_history) { JiraTeamMetrics::SyncHistory.create(jira_board_id: other_jira_id) }
+      let!(:other_board_fragment) do
+        travel_to created_time + 1 do
+          JiraTeamMetrics::ReportFragment.create(
+            report_key: 'my_report',
+            fragment_key: 'my_fragment',
+            contents: 'Other Contents',
+            sync_history: other_sync_history)
+        end
+      end
+
+      it "returns the fragment for the expect board" do
+        fragment1 = JiraTeamMetrics::ReportFragment.fetch(jira_id, 'my_report', 'my_fragment')
+        fragment2 = JiraTeamMetrics::ReportFragment.fetch(other_jira_id, 'my_report', 'my_fragment')
+
+        expect(fragment1).to eq(my_fragment)
+        expect(fragment2).to eq(other_board_fragment)
+      end
     end
   end
 
   describe ".fetch_contents" do
     it "returns the contents for the given report_key and fragment_key, if the fragment exists" do
-      contents = JiraTeamMetrics::ReportFragment.fetch_contents('my_report', 'my_fragment')
+      contents = JiraTeamMetrics::ReportFragment.fetch_contents(jira_id, 'my_report', 'my_fragment')
       expect(contents).to eq(my_fragment_contents)
     end
 
     it "returns the contents matching the report_key, fragment_key and sync_history_id, if the fragment exists" do
-      contents = JiraTeamMetrics::ReportFragment.fetch_contents('my_report', 'my_fragment', old_sync_history.id)
+      contents = JiraTeamMetrics::ReportFragment.fetch_contents(jira_id,'my_report', 'my_fragment', old_sync_history.id)
       expect(contents).to eq(my_old_fragment_contents)
     end
 
     it "returns nil otherwise" do
-      contents = JiraTeamMetrics::ReportFragment.fetch_contents('my_report', 'not_a_fragment')
+      contents = JiraTeamMetrics::ReportFragment.fetch_contents(jira_id,'my_report', 'not_a_fragment')
       expect(contents).to eq(nil)
     end
   end
