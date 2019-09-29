2b430c800f94c96473abe826f9c670fa720aface
John Brunton <jbrunton@zipcar.co.uk>
2019-09-29 16:49:51 -0400

    add a new component

--- jira-team-metrics-ui/src/router/index.js
+++ jira-team-metrics-ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Domain from '@/components/Domain'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/domain',
+      name: 'Domain',
+      component: Domain
     }
   ]
 })
